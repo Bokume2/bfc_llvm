f4732dfc7d26fa1bb2cca400a9a35028c687352f
Bokume2 <nagatomoyuu2@gmail.com>
2026-04-27 18:50:57 +0900

    feat: llcおよびccの自動呼び出し機能とLLVM IRのファイル出力機能を追加

--- README.md
+++ README.md
@@ -23,23 +23,33 @@
    cd bfc_llvm
    go build ./cmd/bfc
    ```
-1. Brainf\*ckのソースコードをテキストファイルに記述し、ビルドしたBrainf\*ckコンパイラに渡します。コンパイルされたLLVM IRは標準出力に出力されるため、適宜リダイレクト等してください。  
-   ```bash
-   ./bfc hoge.bf > hoge.ll
-   ```
-   オプションとして、LLVMの最適化のレベルを`-O0`～`-O3`で指定することが可能です。デフォルトは`-O3`で、レベルごとの最適化の内容はLLVMの各種コマンドと同じです。  
-   ```bash
-   ./bfc -O1 hoge.bf > hoge.ll
-   ```
-1. 生成したLLVM IRをClangで実行可能ファイルにコンパイルします。ターゲットマシンの上書き警告(`Woverride-module`)は基本的に無視して構わないでしょう。  
-   ```bash
-   clang --Wno-override-module -o hoge hoge.ll
-   ```
-   あるいは、`llc`でアセンブリにコンパイルしてから、任意のCコンパイラで実行可能ファイルにコンパイルします。  
-   ```bash
-   llc -o hoge.s hoge.ll
-   cc -o hoge hoge.s
-   ```
+1. 実行可能ファイルを、以下のいずれかの方法で生成します。  
+   - Cコンパイラ等を手動で実行する方法
+     1. Brainf\*ckのソースコードをテキストファイルに記述し、ビルドしたBrainf\*ckコンパイラに渡します。`-o`オプションを省略するとLLVM IRが標準出力に出力されます。  
+        ```bash
+        ./bfc hoge.bf -o hoge.ll
+        ```
+        オプションとして、LLVMの最適化のレベルを`-O0`～`-O3`で指定することが可能です。デフォルトは`-O3`で、レベルごとの最適化の内容はLLVMの各種コマンドと同じです。  
+        ```bash
+        ./bfc -O1 hoge.bf -o hoge.ll
+        ```
+     1. 生成したLLVM IRをClangで実行可能ファイルにコンパイルします。ターゲットマシンの上書き警告(`Woverride-module`)は基本的に無視して構わないでしょう。  
+        ```bash
+        clang --Wno-override-module -o hoge hoge.ll
+        ```
+        あるいは、`llc`でアセンブリにコンパイルしてから、任意のCコンパイラで実行可能ファイルにコンパイルします。  
+        ```bash
+        llc -o hoge.s hoge.ll
+        cc -o hoge hoge.s
+        ```
+   - Cコンパイラまで自動実行する方法
+      1. PATHの通った場所に`llc`の名前でLLVM IRのコンパイラを、`cc`の名前でCコンパイラを用意します(コマンドエイリアス等では正しく動作しません)。  
+      1. `-c`または`--compile-to-exe`フラグを付けてコンパイルします。  
+         `-o`オプションの値は`cc`の`-o`オプションに直接渡されます(お使いの`cc`コマンドの仕様に注意してください)。`-o`オプションを省略すると、ソースファイルのパスから拡張子を消したパスを`cc`の`-o`オプションに渡します。  
+         ```bash
+         # -oオプションを省略した場合は`-o hoge`と同じ
+         ./bfc --compile-to-exe hoge.bf -o hoge
+         ```
 1. 実行可能ファイルを実行します。  
    ```bash
    ./hoge
--- cmd/bfc/bfc.go
+++ cmd/bfc/bfc.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"os/exec"
 	"regexp"
 	"strconv"
 
@@ -12,11 +14,23 @@ import (
 )
 
 func main() {
+	const llcCmd = "llc"
+	const ccCmd = "cc"
+
 	srcFilePath := ""
 	optLv := 3
 	optRE := regexp.MustCompile(`-O(\d)`)
+	toCompile := false
+	outFilePath := ""
+	switchOutFile := false
 	for i, arg := range os.Args {
 		if i == 0 {
+			continue
+		}
+
+		if switchOutFile {
+			outFilePath = arg
+			switchOutFile = false
 			continue
 		}
 
@@ -26,6 +40,10 @@ func main() {
 				fmt.Fprintln(os.Stderr, "Error:", "Optimization "+arg+" is not supported")
 				os.Exit(1)
 			}
+		} else if arg == "-c" || arg == "--compile-to-exe" {
+			toCompile = true
+		} else if arg == "-o" {
+			switchOutFile = true
 		} else if srcFilePath == "" {
 			srcFilePath = arg
 		} else {
@@ -55,5 +73,54 @@ func main() {
 	defer m.Context().Dispose()
 	defer m.Dispose()
 	llvm.OptimizeIR(m, uint(optLv))
-	fmt.Println(m)
+
+	if !toCompile {
+		if outFilePath != "" {
+			irFile, err := os.Create(outFilePath)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Error:", "Cannot write to "+outFilePath)
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+			if _, err := irFile.WriteString(m.String()); err != nil {
+				fmt.Fprintln(os.Stderr, "Error:", "Cannot write to "+outFilePath)
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+		} else {
+			fmt.Println(m)
+		}
+		return
+	}
+
+	exeFilePath := regexp.MustCompile(`\.[\dA-Za-z]*?$`).ReplaceAllString(srcFilePath, "")
+	asmFilePath := exeFilePath + ".s"
+
+	if outFilePath != "" {
+		exeFilePath = outFilePath
+	}
+
+	compileIR := exec.Command(llcCmd, "-o", asmFilePath)
+	compileIRStdin, err := compileIR.StdinPipe()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer compileIRStdin.Close()
+	io.WriteString(compileIRStdin, m.String())
+	compileIRStdin.Close()
+	if err := compileIR.Run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	if err := exec.Command(ccCmd, "-o", exeFilePath, asmFilePath).Run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	if err := os.Remove(asmFilePath); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
